2e645f74dc2a00307c0c6e978f4a82e4053da2f5
tjeerd verschragen <t.verschragen@krm.nl>
2021-05-12 18:35:04 +0200

    KVS-2: Remove accidental KVS-3 improvement

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,18 @@
-import PairController from '@src/controllers/PairController';
+import { getPair } from '@src/models/pairs';
 
 const routes = (route) => {
 	route.get('/', (req, res) => {
 		res.sendFile(`${__dirname}/client/index.html`);
 	});
-	route.get('/pairs/:key', PairController.getByKey);
+	route.get('/pairs/:key', async (req, res) => {
+		const { key } = req.params;
+		const pairs = await getPair(key);
+
+		if (pairs) {
+			res.send({ detail: `pair not found with key: ${key}` });
+		}
+		res.send(pairs[0]);
+	});
 };
 
 export default routes;
